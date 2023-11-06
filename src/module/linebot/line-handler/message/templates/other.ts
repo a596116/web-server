6a0ac5c3c17dc83ea5583efb8bf6b0c9aeb9be65
haodai <haodai.wang@sshop.com.tw>
2023-11-06 15:02:56 +0800

    ? map

--- src/module/linebot/line-handler/message/templates/other.ts
+++ src/module/linebot/line-handler/message/templates/other.ts
@@ -1,16 +1,20 @@
-import { ImageMapMessage } from "@line/bot-sdk"
+import { ImageMapMessage } from '@line/bot-sdk'
 
 export const otherTemplate: ImageMapMessage = {
-    type: 'imagemap',
-    baseUrl: 'https://upload.cc/i1/2022/09/29/v3GJcO.jpg?',
-    altText: 'Menus ImageMap',
-    baseSize: { width: 2500, height: 1686 },
-    actions: [
-        { area: { x: 0, y: 0, width: 833, height: 843 }, type: 'message', text: 'Menus' },
-        { area: { x: 833, y: 0, width: 833, height: 843 }, type: 'message', text: 'Github' },
-        { area: { x: 1666, y: 0, width: 833, height: 843 }, type: 'message', text: 'Articles' },
-        { area: { x: 0, y: 843, width: 833, height: 843 }, type: 'message', text: 'Lists' },
-        { area: { x: 833, y: 843, width: 833, height: 843 }, type: 'message', text: 'Links' },
-        { area: { x: 1666, y: 843, width: 833, height: 843 }, type: 'message', text: 'Setting' },
-    ],
+  type: 'imagemap',
+  baseUrl: 'https://i.imgur.com/kFzv5DA.png?',
+  altText: '功能選單',
+  baseSize: { width: 2500, height: 1686 },
+  actions: [
+    { area: { x: 0, y: 0, width: 833, height: 843 }, type: 'message', text: 'none' },
+    { area: { x: 833, y: 0, width: 833, height: 843 }, type: 'message', text: 'none' },
+    {
+      area: { x: 1666, y: 0, width: 833, height: 843 },
+      type: 'uri',
+      linkUri: 'https://liff.line.me/1657518005-n0BMBGqY',
+    },
+    { area: { x: 0, y: 843, width: 833, height: 843 }, type: 'message', text: 'nike' },
+    { area: { x: 833, y: 843, width: 833, height: 843 }, type: 'message', text: 'hypebeast' },
+    { area: { x: 1666, y: 843, width: 833, height: 843 }, type: 'message', text: 'ithome' },
+  ],
 }
